51dd25b59a5f84483a197d614fa8b30fd9fc6ce2
Josh Stark <jagfin1@gmail.com>
2015-10-27 21:22:50 +0000

    Removed check during install so it always pulls down latest jar file on start

--- init/30_install_davos.sh
+++ init/30_install_davos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-[[ ! -f /app/davos.jar ]] && curl -o /app/davos.jar http://ci.linuxserver.io:8080/job/Davos/job/Release/ws/build/libs/davos-0.0.1-SNAPSHOT.jar
+curl -o /app/davos.jar http://ci.linuxserver.io:8080/job/Davos/job/Release/ws/build/libs/davos-0.0.1-SNAPSHOT.jar
 
 chown abc:abc /app/davos.jar
