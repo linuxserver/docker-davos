043503dca6c366a0bcad5a5299f7ba41a7372fad
Josh Stark <JAGFin1@users.noreply.github.com>
2015-11-10 18:37:23 +0000

    Update 30_install_davos.sh
    References davos-1.0.jar now

--- init/30_install_davos.sh
+++ init/30_install_davos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -o /app/davos.jar http://ci.linuxserver.io:8080/job/Davos/job/Release/ws/build/libs/davos-0.0.1-SNAPSHOT.jar
+curl -o /app/davos.jar http://ci.linuxserver.io:8080/job/Davos/job/Release/ws/build/libs/davos-1.0.jar
 
 chown abc:abc /app/davos.jar
