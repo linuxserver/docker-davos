0accef4022da7dd722e8e461d8cfdc34ffed339f
Josh Stark <JAGFin1@users.noreply.github.com>
2016-04-03 17:55:41 +0100

    Update 30_install_davos.sh
    Changed location of built application

--- init/30_install_davos.sh
+++ init/30_install_davos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -o /app/davos.jar http://ci.linuxserver.io:8080/job/Davos/job/Release/ws/build/libs/davos-1.0.jar
+curl -o /app/davos.jar https://ci.linuxserver.io/job/davos/job/davos_03_Release/ws/build/libs/davos-1.0.jar
 
 chown abc:abc /app/davos.jar
